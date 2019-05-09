8ff8bb2e22335f9fe7e3a927e651c6ed168d454a
ljayden <ljayden330@gmail.com>
2019-05-09 22:40:36 +0900

    Add remote redis connect AND Polishing

--- src/main/java/com/uneedcomms/streamdataapi/repository/PermissionsRepository.java
+++ src/main/java/com/uneedcomms/streamdataapi/repository/PermissionsRepository.java
@@ -4,6 +4,4 @@ import com.uneedcomms.streamdataapi.domain.Permissions;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PermissionsRepository extends CrudRepository<Permissions, String> {
-
-    Permissions findByKey(String key);
 }
--- src/main/java/com/uneedcomms/streamdataapi/runner/RedisTestRunner.java
+++ src/main/java/com/uneedcomms/streamdataapi/runner/RedisTestRunner.java
@@ -27,9 +27,13 @@ public class RedisTestRunner implements ApplicationRunner {
         permissions.setKey("8122f28a-1481-4471-a1a4-a7fddbc30ac1");
         permissions.setStatus("inactive");
         Permissions savedPermissions02 = permissionsRepository.save(permissions);
+        permissions.setKey("1234-1234-1234");
+        permissions.setStatus("inactive");
+        Permissions savedPermissions03 = permissionsRepository.save(permissions);
 
         System.out.println(savedPermissions01);
         System.out.println(savedPermissions02);
+        System.out.println(savedPermissions03);
 
         String key = "b8de2234-085c-43f5-b1ea-31d685dcafa1";
 //        Permissions findedPermissions = permissionsRepository.findByKey(key);
